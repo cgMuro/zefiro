d07eca8d233c691360e969a757cba16c4837848b
Gio <cmurogio@gmail.com>
2020-06-16 19:50:17 +0200

    showing content only when user is defined

--- frontend/src/components/user/User.js
+++ frontend/src/components/user/User.js
@@ -44,22 +44,29 @@ export default function User(props) {
                     :
                     null
             }
-            <Link to={`/user/${props.match.params.id}/settings`} className="linkStyle">
-                <span id="user-settings"><i className="fa fa-cog fa-lg"></i></span>
-            </Link>
-            <Container className="d-flex justify-content-center align-items-center flex-column" style={{ height: '80vh' }} id="user-page-container">
-                <h2 className="text-center mb-3">Welcome <span style={{ fontWeight: 'bold' }}>{user ? user.name : null}</span></h2>
-                <Link to={`/user/${props.match.params.id}/articles`} className="linkStyle">
-                    <Row className="mb-2 user-row text-center">
-                        <h5>SEE YOUR ARTICLES</h5>
-                    </Row>
-                </Link>
-                <Link to={`/user/${props.match.params.id}/create-article`} className="linkStyle">
-                    <Row className="user-row text-center">
-                        <h5>CREATE NEW ARTICLE</h5>
-                    </Row>
-                </Link>
-            </Container>
+            {
+                user
+                    ?
+                    <>
+                        <Link to={`/user/${props.match.params.id}/settings`} className="linkStyle">
+                            <span id="user-settings"><i className="fa fa-cog fa-lg"></i></span>
+                        </Link>
+                        <Container className="d-flex justify-content-center align-items-center flex-column" style={{ height: '80vh' }} id="user-page-container">
+                            <h2 className="text-center mb-3">Welcome <span style={{ fontWeight: 'bold' }}>{user ? user.name : null}</span></h2>
+                            <Link to={`/user/${props.match.params.id}/articles`} className="linkStyle">
+                                <Row className="mb-2 user-row text-center">
+                                    <h5>SEE YOUR ARTICLES</h5>
+                                </Row>
+                            </Link>
+                            <Link to={`/user/${props.match.params.id}/create-article`} className="linkStyle">
+                                <Row className="user-row text-center">
+                                    <h5>CREATE NEW ARTICLE</h5>
+                                </Row>
+                            </Link>
+                        </Container>
+                    </>
+                    : null
+            }
         </>
     )
 }
